ec32cf57312c4bded6c8dab398a59314b7a9789e
acs0209 <annjae4676@naver.com>
2023-08-28 20:36:00 +0900

    배포 테스트2

--- blog/src/test/java/sgdevcamp/blog/BlogApplicationTests.java
+++ blog/src/test/java/sgdevcamp/blog/BlogApplicationTests.java
@@ -1,9 +1,12 @@
 package sgdevcamp.blog;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Test
+	void contextLoads() {
+	}
 }
