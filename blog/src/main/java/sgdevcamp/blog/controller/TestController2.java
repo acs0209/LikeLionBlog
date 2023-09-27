2ea5cc4726a78076ca552819bb6931190743e44c
acs0209 <annjae4676@naver.com>
2023-09-27 17:10:48 +0900

    mh

--- blog/src/main/java/sgdevcamp/blog/controller/TestController2.java
+++ blog/src/main/java/sgdevcamp/blog/controller/TestController2.java
@@ -7,8 +7,8 @@ public class TestController2 {
     private int num45;
     private int num65;
     private int num75;
-    private int num71235;
-    private int num75123;
+    private int num71235sdfhsakjfhskajhsdkjfh;
+    private int num7512sdjfhsakjdfhskjdfh3;
     private int num7123werwer123125123;
     private int num751123123werwerwer23;
 }
