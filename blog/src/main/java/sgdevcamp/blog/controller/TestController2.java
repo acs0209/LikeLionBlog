bb23d0ec93c18e9f5b733d3cbbd708e20feae7b9
acs0209 <annjae4676@naver.com>
2023-09-27 17:14:19 +0900

    mh

--- blog/src/main/java/sgdevcamp/blog/controller/TestController2.java
+++ blog/src/main/java/sgdevcamp/blog/controller/TestController2.java
@@ -2,13 +2,10 @@ package sgdevcamp.blog.controller;
 
 public class TestController2 {
     private int num;
-    private int num2;
+    private int num1231232;
     private int num3;
-    private int num45;
-    private int num65;
-    private int num75;
+    private int fsdf45;
+    private int zxcvxv;
     private int num71235sdfhsakjfhskajhsdkjfh;
-    private int num7512sdjfhsakjdfhskjdfh3;
-    private int num7123werwer123125123;
     private int num751123123werwerwer23;
 }
